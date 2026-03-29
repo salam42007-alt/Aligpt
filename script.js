0e59458d95bd287e3caf4b17e1ea8fbbcb0da89a
salam42007-alt <salam42007@gmail.com>
2026-03-28 23:23:42 -1200

    Update script.js

--- script.js
+++ script.js
@@ -9,7 +9,7 @@
 
 const SUPABASE_URL  = 'https://hjhtwurqcectnjpudsxl.supabase.co';
 const SUPABASE_KEY  = 'sb_publishable_XP6PQTRgqfeDr1ItTkbWdA_Goe1Vafo';
-const GEMINI_API_KEY = 'YOUR_GEMINI_API_KEY';
+const GEMINI_API_KEY = 'AIzaSyCHRAAAv298SEvIU0RnPKq537JXf4SKYRk';
 
 // Demo mode (no real keys needed to test)
 const DEMO_MODE = false;
